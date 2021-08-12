1623eb4b4dec95a98fe5427633a4ee32e8c1e65d
Amos Ahounou <amosahounou@gmail.com>
2021-08-13 00:12:05 +0100

    Authentifcation avec fb OAuth2.0 passport facebook token

--- authenticate.js
+++ authenticate.js
@@ -5,6 +5,7 @@ var User = require('./models/user');// Le schema user de mongoDB
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var FacebookTokenStrategy = require('passport-facebook-token');
 
 var config = require('./config.js');
 
@@ -63,3 +64,32 @@ exports.verifyAdmin=(req,res,next)=> {
         return next(err);
     }
 };
+
+/***Passport facebook*/
+exports.facebookPassport = passport.use(new FacebookTokenStrategy({
+    clientID: config.facebook.clientId,
+    clientSecret: config.facebook.clientSecret
+}, /**Call back fonction*/ (accessToken, refreshToken, profile, done) => {
+        User.findOne({facebookId: profile.id}, (err, user) => {
+            if (err) {
+                return done(err, false);
+            }
+            if (!err && user !== null) {
+                return done(null, user);
+            }
+            else {
+                /**Le nom sera obtenu de Facebook  pour les donnes on utilise profile*/
+                user = new User({ username: profile.displayName });
+                user.facebookId = profile.id;
+                user.firstname = profile.name.givenName;
+                user.lastname = profile.name.familyName;
+                user.save((err, user) => {
+                    if (err)
+                        return done(err, false);
+                    else
+                        return done(null, user);
+                })
+            }
+        });
+    }
+    ));
